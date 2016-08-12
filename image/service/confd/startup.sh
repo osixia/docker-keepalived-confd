fc8ef5e129ec31b7901b3a04502d09d6d5fc9557
ofreax <bertrand.gouny@gmail.com>
2016-08-12 14:24:01 +0200

    add ssl-helper for confd client

--- image/service/confd/startup.sh
+++ image/service/confd/startup.sh
@@ -16,4 +16,18 @@ ln -sf ${CONTAINER_SERVICE_DIR}/confd/assets/templates/* /etc/confd/templates/
 # override the keepalived image config file with the confd template
 ln -sf ${CONTAINER_SERVICE_DIR}/confd/assets/templates/keepalived.tmpl ${CONTAINER_SERVICE_DIR}/keepalived/assets/keepalived.conf
 
+
+FIRST_START_DONE="${CONTAINER_STATE_DIR}/docker-confd-first-start-done"
+# container first start
+if [ ! -e "$FIRST_START_DONE" ]; then
+
+  if [ "${KEEPALIVED_CONFD_CLIENT_TLS,,}" == "true" ]; then
+    # backend ssl
+    # generate a certificate and key if files don't exists
+    # https://github.com/osixia/docker-light-baseimage/blob/stable/image/service-available/:ssl-tools/assets/tool/ssl-helper
+    ssl-helper ${KEEPALIVED_CONFD_CLIENT_SSL_HELPER_PREFIX} "${CONTAINER_SERVICE_DIR}/confd/assets/certs/$KEEPALIVED_CONFD_CLIENT_CERT_FILENAME" "${CONTAINER_SERVICE_DIR}/confd/assets/certs/$KEEPALIVED_CONFD_CLIENT_KEY_FILENAME" "${CONTAINER_SERVICE_DIR}/confd/assets/certs/$KEEPALIVED_CONFD_CLIENT_CAKEYS_FILENAME"
+  fi
+  touch $FIRST_START_DONE
+fi
+
 exit 0
